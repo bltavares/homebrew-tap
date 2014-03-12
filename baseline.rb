e506c1646b6fd9c2a443f8d47eee1a40a3f2e29f
Bruno Tavares <bruno.exz@gmail.com>
2014-03-12 18:16:03 -0300

    Add --devel option to baseline

--- baseline.rb
+++ baseline.rb
@@ -5,6 +5,11 @@ class Baseline < Formula
   url "https://github.com/bltavares/baseline/archive/1.3.0.tar.gz"
   sha1 "27429f5f3e34b048ab19dc6d56afa12f913c60e5"
 
+  devel do
+    url "https://github.com/bltavares/baseline.git", :using => :git, :branch => "kickstart"
+    version "2.0.0-dev"
+  end
+
   def install
     prefix.install Dir['*']
   end
